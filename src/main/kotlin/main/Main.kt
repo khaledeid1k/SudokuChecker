bc48bc9b751b1c43d0454cd24959cbd1ace26ad0
Khaled.Eid <Khaled.Eid@souhoola.com>
2025-04-04 18:22:29 +0200

    update test use case

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -7,67 +7,88 @@ fun main() {
         caseName = "when no repeated number at row and at column should return true",
         actualResult = checkSudokuValidation(
             listOf(
-                listOf("1", "2", "3"),
-                listOf("3", "1", "2"),
-                listOf("2", "3", "1")
+                listOf("5", "3", "4", "6", "7", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "8"),
+                listOf("1", "9", "8", "3", "4", "2", "5", "6", "7"),
+                listOf("8", "5", "9", "7", "6", "1", "4", "2", "3"),
+                listOf("4", "2", "6", "8", "5", "3", "7", "9", "1"),
+                listOf("7", "1", "3", "9", "2", "4", "8", "5", "6"),
+                listOf("9", "6", "1", "5", "3", "7", "2", "8", "4"),
+                listOf("2", "8", "7", "4", "1", "9", "6", "3", "5"),
+                listOf("3", "4", "5", "2", "8", "6", "1", "7", "9")
             )
         ),
         expectedResult = true
     )
     checkResult(
-        caseName = "when no repeated number at row and at column 4*4 should return true",
+        caseName = "when pass alphabetical char should return false",
         actualResult = checkSudokuValidation(
             listOf(
-                listOf("1", "2", "3","4"),
-                listOf("3", "1", "4","2"),
-                listOf("4", "3", "2","1"),
-                listOf("2", "4", "1","3"),
+                listOf("5", "3", "a", "6", "7", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "8"),
+                listOf("1", "9", "8", "3", "4", "2", "5", "6", "7"),
+                listOf("8", "5", "9", "7", "6", "1", "4", "2", "3"),
+                listOf("4", "2", "6", "8", "5", "3", "7", "9", "1"),
+                listOf("7", "1", "3", "9", "2", "4", "8", "5", "6"),
+                listOf("9", "6", "1", "5", "3", "7", "2", "8", "4"),
+                listOf("2", "8", "7", "4", "1", "9", "6", "3", "5"),
+                listOf("3", "4", "5", "2", "8", "6", "1", "7", "9")
             )
         ),
-        expectedResult = true
+        expectedResult = false
     )
     checkResult(
-        caseName = "when no repeated number at row and at column 5x5 should return true",
+        caseName = "when enter an empty sudoku should return false",
         actualResult = checkSudokuValidation(
             listOf(
-                listOf("1", "2", "3", "4", "5"),
-                listOf("2", "3", "4", "5", "1"),
-                listOf("3", "4", "5", "1", "2"),
-                listOf("4", "5", "1", "2", "3"),
-                listOf("5", "1", "2", "3", "4")
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-")
             )
         ),
-        expectedResult = true
+        expectedResult = false
     )
 
     checkResult(
-        caseName = "when no repeated number at row and at column 9x9 should return true",
+        caseName = "when sudoku contains repeated numbers in the same row should return true",
         actualResult = checkSudokuValidation(
             listOf(
-                listOf("1", "2", "3", "4", "5", "6", "7", "8", "9"),
-                listOf("2", "3", "4", "5", "6", "7", "8", "9", "1"),
-                listOf("3", "4", "5", "6", "7", "8", "9", "1", "2"),
-                listOf("4", "5", "6", "7", "8", "9", "1", "2", "3"),
-                listOf("5", "6", "7", "8", "9", "1", "2", "3", "4"),
-                listOf("6", "7", "8", "9", "1", "2", "3", "4", "5"),
-                listOf("7", "8", "9", "1", "2", "3", "4", "5", "6"),
-                listOf("8", "9", "1", "2", "3", "4", "5", "6", "7"),
-                listOf("9", "1", "2", "3", "4", "5", "6", "7", "8")
+                listOf("5", "3", "4", "6", "7", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "8"),
+                listOf("1", "9", "8", "3", "7", "2", "5", "6", "7"),
+                listOf("8", "5", "9", "7", "6", "1", "4", "2", "3"),
+                listOf("4", "2", "6", "8", "5", "3", "7", "9", "1"),
+                listOf("7", "1", "3", "9", "2", "4", "8", "5", "6"),
+                listOf("9", "6", "1", "5", "3", "7", "2", "8", "4"),
+                listOf("2", "8", "7", "4", "1", "9", "6", "3", "5"),
+                listOf("3", "4", "5", "2", "8", "6", "1", "7", "3")
             )
         ),
-        expectedResult = true
+        expectedResult = false
     )
 
 
 
 
     checkResult(
-        caseName = "when repeated number at row or at column should return false",
+        caseName = "when sudoku contains repeated numbers in the same column should return false",
         actualResult = checkSudokuValidation(
             listOf(
-                listOf("1", "1", "3"),
-                listOf("3", "2", "1"),
-                listOf("2", "3", "1")
+                listOf("5", "3", "4", "6", "7", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "8"),
+                listOf("1", "9", "8", "3", "4", "3", "5", "6", "7"),
+                listOf("8", "5", "9", "7", "6", "1", "4", "2", "3"),
+                listOf("4", "2", "6", "8", "5", "3", "7", "9", "1"),
+                listOf("7", "1", "3", "9", "2", "4", "8", "5", "6"),
+                listOf("9", "6", "2", "5", "3", "7", "2", "8", "4"),
+                listOf("2", "8", "7", "4", "1", "9", "6", "3", "5"),
+                listOf("3", "4", "5", "2", "8", "6", "1", "7", "9")
             )
         ),
         expectedResult = false
@@ -77,31 +98,33 @@ fun main() {
         caseName = "When any symbol except '-' is passed should return false.",
         actualResult = checkSudokuValidation(
             listOf(
-                listOf("5", "/", "-"),
-                listOf("3", "1", "2"),
-                listOf("2", "3", "1")
+                listOf("5", "3", "4", "6", "/", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "8"),
+                listOf("1", "9", "8", "3", "4", "2", "5", "6", "7"),
+                listOf("8", "5", "9", "7", "6", "1", "4", "2", "3"),
+                listOf("4", "2", "6", "8", "5", "3", "7", "9", "1"),
+                listOf("7", "1", "3", "9", "2", "4", "8", "5", "6"),
+                listOf("9", "6", "1", "5", "3", "7", "2", "8", "4"),
+                listOf("2", "8", "7", "4", "1", "9", "6", "3", "5"),
+                listOf("3", "4", "5", "2", "8", "6", "1", "7", "9")
             )
         ),
         expectedResult = false
     )
-    checkResult(
-        caseName = "when pass alphabetical char should return false",
-        actualResult = checkSudokuValidation(
-            listOf(
-                listOf("5", "e", "-"),
-                listOf("3", "1", "2"),
-                listOf("2", "3", "1")
-            )
-        ),
-        expectedResult = false
-    )
+
     checkResult(
         caseName = "when pass space should return false",
         actualResult = checkSudokuValidation(
             listOf(
-                listOf("5", " ", "-"),
-                listOf("3", "1", "2"),
-                listOf("2", "3", "1")
+                listOf("5", "3", " ", "6", "7", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "8"),
+                listOf("1", "9", "8", "3", "4", "2", "5", "6", "7"),
+                listOf("8", "5", "9", "7", "6", "1", "4", "2", "3"),
+                listOf("4", "2", "6", "8", "5", "3", "7", "9", "1"),
+                listOf("7", "1", "3", "9", "2", "4", "8", "5", "6"),
+                listOf("9", "6", "1", "5", "3", "7", "2", "8", "4"),
+                listOf("2", "8", "7", "4", "1", "9", "6", "3", "5"),
+                listOf("3", "4", "5", "2", "8", "6", "1", "7", "9")
             )
         ),
         expectedResult = false
@@ -110,20 +133,15 @@ fun main() {
         caseName = "when grid contains numbers outside the range should return false", // write range at use case
         actualResult = checkSudokuValidation(
             listOf(
-                listOf("1", "1", "4"),
-                listOf("3", "2", "1"),
-                listOf("2", "3", "1")
-            )
-        ),
-        expectedResult = false
-    )
-    checkResult(
-        caseName = "when board is empty list should return false",
-        actualResult = checkSudokuValidation(
-            listOf(
-                listOf("-", "-", "-"),
-                listOf("-", "-", "-"),
-                listOf("-", "-", "-"),
+                listOf("5", "3", "4", "20", "7", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "8"),
+                listOf("1", "9", "8", "3", "4", "2", "5", "6", "7"),
+                listOf("8", "5", "9", "7", "6", "1", "4", "2", "3"),
+                listOf("4", "2", "6", "8", "5", "3", "7", "9", "1"),
+                listOf("7", "1", "3", "9", "2", "4", "8", "5", "6"),
+                listOf("9", "6", "1", "5", "3", "7", "2", "8", "4"),
+                listOf("2", "8", "7", "4", "1", "9", "6", "3", "5"),
+                listOf("3", "4", "5", "2", "8", "6", "1", "7", "9")
             )
         ),
         expectedResult = false
@@ -132,9 +150,15 @@ fun main() {
         caseName = "when grid contains negative numbers should return false",
         actualResult = checkSudokuValidation(
             listOf(
-                listOf("1", "-1", "4"),
-                listOf("3", "2", "1"),
-                listOf("2", "3", "1")
+                listOf("5", "3", "4", "-1", "7", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "8"),
+                listOf("1", "9", "8", "3", "4", "2", "5", "6", "7"),
+                listOf("8", "5", "9", "7", "6", "1", "4", "2", "3"),
+                listOf("4", "2", "6", "8", "5", "3", "7", "9", "1"),
+                listOf("7", "1", "3", "9", "2", "4", "8", "5", "6"),
+                listOf("9", "6", "1", "5", "3", "7", "2", "8", "4"),
+                listOf("2", "8", "7", "4", "1", "9", "6", "3", "5"),
+                listOf("3", "4", "5", "2", "8", "6", "1", "7", "9")
             )
         ),
         expectedResult = false
