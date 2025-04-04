7c41071d79205d0022656e4b19ef1329cf99e4e9
Khaled.Eid <Khaled.Eid@souhoola.com>
2025-04-04 05:27:55 +0200

    refactoring code

--- src/main/kotlin/main/CheckSudokuValidation.kt
+++ src/main/kotlin/main/CheckSudokuValidation.kt
@@ -1,23 +1,15 @@
 package main
 
 fun checkSudokuValidation(sudokuNumbers: List<List<String>>): Boolean {
-
-    return checkSudokuRules(sudokuNumbers)
-}
-
-
-
-fun checkSudokuRules(grid: List<List<String>>): Boolean {
     val validChars = listOf("-","1", "2", "3")
-    if (grid.any { row -> row.any { it !in validChars } }) return false
-    for (row in grid) {
+    if (sudokuNumbers.any { row -> row.any { it !in validChars } }) return false
+    for (row in sudokuNumbers) {
         if (!isValidUnit(row)) return false
     }
     for (col in 0..2) {
-        val column = listOf(grid[0][col], grid[1][col], grid[2][col])
+        val column = listOf(sudokuNumbers[0][col], sudokuNumbers[1][col], sudokuNumbers[2][col])
         if (!isValidUnit(column)) return false
     }
-
     return true
 }
 
